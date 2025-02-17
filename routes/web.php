d62fc2b3f0eba5e0ea17746a600952b4f1cd5dc0
rajwakhowaridzmi <rajwakhowaridzmi07@gmail.com>
2025-02-17 22:26:23 +0700

    Pemasangan Template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Livewire\Dashboard;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
+
+Route::get('/dashboard', Dashboard::class)->name('dashboard');
